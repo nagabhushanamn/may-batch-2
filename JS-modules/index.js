a82cfaae59973ee5ee91ee520dca133634450880
Nagabhushanam <nagabhushanamn@gmail.com>
2018-05-11 11:29:36 +0530

    simple-web-ui

--- JS-modules/index.js
+++
@@ -1,3 +0,0 @@
-
-
-import item1, { item2, item3 } from './hotel/menu';
---
+++ simple-web-ui/app.js
@@ -0,0 +1,57 @@
+console.log('-app.js-');
+
+
+// using DOM API
+
+// query DOM & bind event-listener(s) & process event
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    let alertBox = document.querySelector('.alert');
+    let showBtn = document.querySelector('.btn-primary');
+    let hideBtn = document.querySelector('.btn-danger');
+    let greetMeBtn = document.querySelector('.btn-success');
+
+    hideBtn.addEventListener('click', function () {
+        alertBox.style.display = 'none';
+    });
+    showBtn.addEventListener('click', function () {
+        alertBox.style.display = 'block';
+    });
+    greetMeBtn.addEventListener('click', function () {
+        let timeNow = new Date().toTimeString()
+        alertBox.innerText = `good morning - ${timeNow}`
+    });
+
+    //-----------------------------------------------
+
+    let images = [
+        "images/1.jpeg",
+        "images/2.jpeg",
+        "images/3.jpeg",
+        "images/4.jpeg",
+        "images/5.jpeg"
+    ];
+
+    let startBtn = document.getElementById('startBtn');
+    let stopBtn = document.getElementById('stopBtn');
+    let imageElement = document.getElementById('slideShowEle');
+
+    startBtn.addEventListener('click', () => {
+        // timer API
+        let idx = 0;
+        let interval = setInterval(() => {
+            let imagePath = images[idx];
+            imageElement.src = imagePath;
+            idx++;
+            if (idx === images.length) idx = 0;
+        }, 1000);
+        stopBtn.addEventListener('click', () => {
+            clearInterval(interval);
+        })
+
+    });
+
+
+
+})
